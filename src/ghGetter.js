d21f17cb6246a6e518749c9c9347e072c7916037
unknown <jussi.kaatiala@gmail.com>
2020-03-21 12:25:43 +0200

    Baseurl separated from the code

--- src/ghGetter.js
+++ src/ghGetter.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
+const baseUrl = 'https://api.github.com'
+
 const getRepos = (userName) => {
-  const request = axios.get(`https://api.github.com/users/${userName}/repos`)
+  const request = axios.get(`${baseUrl}/users/${userName}/repos`)
   return request.then((response) => response.data)
 }
 
 const getCommits = (userName, repoName) => {
-  const request = axios.get(`https://api.github.com/repos/${userName}/${repoName}/commits`)
+  const request = axios.get(`${baseUrl}/repos/${userName}/${repoName}/commits`)
   return request.then((response) => response.data)
 }
 
