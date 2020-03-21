b353707d42a1e15ab4274a0d2dc20b5f3bdf7e46
juissi999 <jussi.kaatiala@gmail.com>
2020-03-21 23:39:23 +0200

    Date postprosessing function added

--- src/components/Author/index.js
+++ src/components/Author/index.js
@@ -4,9 +4,16 @@ import AuthorAvatar from './Avatar.js'
 import AuthorDate from './Date.js'
 
 const Author = ({author}) => {
+
+  const renderDate = () => {
+    // clear the daystring to be more human readable
+    const d = new Date(author.date)
+    return (d.toLocaleDateString("fi-FI"))
+  }
+
   return (<div className={'author'}>
           <AuthorName name={author.name}/>
-          <AuthorDate date={author.date}/>
+          <AuthorDate date={renderDate(author.date)}/>
           <AuthorAvatar />
           </div>)
 }
