7dc8abf2c551dbebf4693d88870b0643779c49b5
juissi999 <jussi.kaatiala@gmail.com>
2020-03-29 18:57:09 +0300

    Added a case-sensitive username check & print

--- src/components/Repository/List.js
+++ src/components/Repository/List.js
@@ -16,30 +16,38 @@ const RepositoryList = () => {
   const [userName, setUsername] = useState('')
 
   // url parameter
-  const user = useParams().user
+  const rawUser = useParams().user
 
   // get repositories from api when value of user changes
   useEffect(()=> {
-    const request = ghGetter.getRepos(user)
-    request.then((response) => {
-      setUsername(user)
-      setRepos(response)
-    })
-    .catch(error => {
-      setUsername('')
-      setRepos([])
-      clearTimeout(errorTimeout)
-      setErrorMsg(`${error}`)
-      errorTimeout = setTimeout(()=>setErrorMsg(''), 2000)
-    })
-  }, [user])
+
+    // get the actual username (case sensitive)
+    // then when username is gotten get repositories for the user
+    // deal with all the errors in one catch-block
+    ghGetter.getUser(rawUser)
+      .then((userinfo) => {
+        const user = userinfo.login
+        setUsername(user)
+        return ghGetter.getRepos(user)
+      })
+      .then((repositories) => {
+        setRepos(repositories)
+      })
+      .catch((error)=> {
+        setUsername('')
+        setRepos([])
+        clearTimeout(errorTimeout)
+        setErrorMsg(`${error}`)
+        errorTimeout = setTimeout(()=>setErrorMsg(''), 2000)
+      })
+  }, [rawUser])
 
   return (<div>
             <Error>{errorMsg}</Error>
             <UserHeader>{userName}</UserHeader>
             <ul>
               {repos.map((repo, i) => {
-                return(<Repository key={i} user={user} repo={repo}/>)
+                return(<Repository key={i} user={userName} repo={repo}/>)
               })}
             </ul>
           </div>)
